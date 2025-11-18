0128c7a6e7ee4a3280f6c82470a901688a86cbb4
PAAWSA02LC0900 <Duoc@PAAWSA02LC0900.duoc.acad>
2025-11-17 21:07:57 -0300

    avances programacion

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBarPrincipal from './components/navbar';
+
+function HomePrincipal() {
+    return (
+        <div>
+            <NavBarPrincipal/>
+        </div>
+    );
+}
+
+export default HomePrincipal;
